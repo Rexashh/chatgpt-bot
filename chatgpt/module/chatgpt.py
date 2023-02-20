f9712f1d88fe173dd366b781f2fbf304b917579c
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-20 23:40:49 +0700

    tws

--- chatgpt/module/chatgpt.py
+++ chatgpt/module/chatgpt.py
@@ -19,31 +19,27 @@ from chatgpt.module.what import *
 from config import OPENAI_API 
 
 @ren.on_message(filters.command("ask") & filters.private | filters.group)
-async def chatgpt(c: Client, m: Message):
+@ratelimiter
+async def chatbot(c, m):
     if len(m.command) == 1:
-        return await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
-    randydev = m.text.split(" ", maxsplit=1)[1]
+        return await kirimPesan(m, f"Gunakan perintah <code>/{m.command[0]} [pertanyaan]</code> untuk menanyakan pertanyaan menggunakan AI.")
+    pertanyaan = m.text.split(" ", maxsplit=1)[1]
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {OPENAI_API}",
     }
 
     json_data = {
-        "prompt": randydev,
         "model": "text-davinci-003",
-        "temperature": 0.5,
-        "max_tokens": 1024,
-        "n": 1,
-        "stop": None,
-        "temperature": 0.9,
-        "top_p": 0.3,
-        "frequency_penalty": 0.5,
+        "prompt": pertanyaan,
+        "max_tokens": 200,
+        "temperature": 0,
     }
-    ran = await m.reply("Wait a moment looking for your answer..", quote=True)
+    msg = await kirimPesan(m, "Wait a moment looking for your answer..")
     try:
         response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
-        await ran.edit(response["choices"][0]["text"])
+        await editPesan(msg, response["choices"][0]["text"])
     except MessageNotModified:
         pass
     except Exception:
-        await ran.edit("Yahh, sorry i can't get your answer.")
+        await editPesan(msg, "Yahh, sorry i can't get your answer.")
